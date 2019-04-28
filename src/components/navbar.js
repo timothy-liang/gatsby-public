bed236f734b72f981105c9c87261ec397fcbcf19
Tim Liang <timothy.liang@vanderbilt.edu>
2019-04-27 19:00:15 -0500

    navbar prototype

---
+++ src/components/navbar.js
@@ -0,0 +1,62 @@
+import React from "react"
+import styled from "styled-components"
+import "../styles/fonts.css"
+
+const Navbar = styled.div`
+  background-color: AliceBlue;
+
+  margin: 0;
+  padding: 0px 20px;
+
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+`
+
+const NavCluster = styled.div`
+  background-color: Azure;
+
+  padding: 20px;
+`
+
+const NavButton = styled.button`
+  background-color: White;
+
+  border: none;
+  border-bottom 1px solid Black;
+  margin: 20px;
+  padding: 2px 0px;
+
+  font-family: Lato;
+  font-size: 20px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const NavLogo = styled.div`
+  margin: 20px;
+
+  font-family: Lato;
+  font-size: 30px;
+`
+
+export default () => (
+  <Navbar>
+
+    <NavCluster>
+      <NavButton>Work</NavButton>
+    </NavCluster>
+
+    <NavCluster>
+      <NavLogo>Tim Liang</NavLogo>
+    </NavCluster>
+
+    <NavCluster>
+      <NavButton>About</NavButton>
+      <NavButton>Resume</NavButton>
+    </NavCluster>
+
+  </Navbar>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
+import styled from "styled-components"
+import {createGlobalStyle} from "styled-components"
+import Navbar from "../components/navbar"
 
-export default () => <div>Hello world!</div>
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`
+
+export default () => (
+  <React.Fragment>
+    <GlobalStyle />
+    <Navbar />
+  </React.Fragment>
+)
