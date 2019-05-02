b72db57130098d47a6f1d908981f6516c80725c7
Tim Liang <timothy.liang@vanderbilt.edu>
2019-05-02 18:48:16 -0500

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
-module.exports = {
+const config = {
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet",
   ],
 }
+
+if (process.env.CONTEXT === "production") {
+  const googleAnalyticsConfig = {
+    resolve: "gatsby-plugin-google-analytics",
+    options: {
+      trackingId: "UA-139405922-1",
+    },
+  }
+  config.plugins.push(googleAnalyticsConfig)
+}
+
+module.exports = config
