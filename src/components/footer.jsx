fa3912779e5eb3f0bfa5ecb8915a0bc912f99df6
Tim Liang <timothy.liang@vanderbilt.edu>
2019-05-03 19:42:51 -0500

    encapsulate social bar

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import { FaInstagram, FaLinkedinIn } from "react-icons/fa"
-import { MdMailOutline } from "react-icons/md"
+import SocialBar from "./socialBar"
 
 const Copyright = styled.div`
   width: 100%;
@@ -14,47 +13,11 @@ const Copyright = styled.div`
   letter-spacing: 1px;
 `
 
-const SocialMediaBar = styled.div`
-  margin-top: 30px;
-  color: black;
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: center;
-`
-const SocialMediaLink = styled.a`
-  color: gray;
-
-  :visited {
-    color: gray;
-  }
-
-  margin: 10px;
-  font-size: 30px;
-
-  transition: color 0.2s ease;
-
-  @media (hover: hover) {
-    :hover {
-      color: black;
-    }
-  }
-`
-
 const year = new Date().getFullYear()
 
 export default () => (
   <React.Fragment>
-    <SocialMediaBar>
-      <SocialMediaLink href="mailto:timothy.liang@vanderbilt.edu">
-        <MdMailOutline />
-      </SocialMediaLink>
-      <SocialMediaLink href="https://www.linkedin.com/in/timothy-liang-51980a143/">
-        <FaLinkedinIn />
-      </SocialMediaLink>
-      <SocialMediaLink href="https://www.instagram.com/possibly_tim/">
-        <FaInstagram />
-      </SocialMediaLink>
-    </SocialMediaBar>
+    <SocialBar />
     <Copyright>
       {`© ${year} Tim Liang`}
     </Copyright>
---
+++ src/components/socialBar.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+import styled from "styled-components"
+import { FaInstagram, FaLinkedinIn } from "react-icons/fa"
+import { MdMailOutline } from "react-icons/md"
+
+const SocialMediaBar = styled.div`
+  margin-top: 30px;
+  color: black;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+`
+const SocialMediaLink = styled.a`
+  color: gray;
+
+  :visited {
+    color: gray;
+  }
+
+  margin: 10px;
+  font-size: 30px;
+
+  transition: color 0.2s ease;
+
+  @media (hover: hover) {
+    :hover {
+      color: black;
+    }
+  }
+`
+
+export default () => (
+  <SocialMediaBar>
+    <SocialMediaLink href="mailto:timothy.liang@vanderbilt.edu">
+      <MdMailOutline />
+    </SocialMediaLink>
+    <SocialMediaLink href="https://www.linkedin.com/in/timothy-liang-51980a143/">
+      <FaLinkedinIn />
+    </SocialMediaLink>
+    <SocialMediaLink href="https://www.instagram.com/possibly_tim/">
+      <FaInstagram />
+    </SocialMediaLink>
+  </SocialMediaBar>
+)
